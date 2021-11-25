eb07a6ff49c0070b374f8c3e45119d8b1d22f83f
Paserno <paserno.x@gmail.com>
2021-11-25 03:11:54 -0300

    Primer Commit

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,18 @@
+// Variables y Constantes
+
+const nombre = 'Paserno';
+const apelloido = 'Rojas';
+
+
+let valorDado = 5;
+valorDado= 2;
+
+console.log(nombre, apelloido, valorDado);
+
+if (true) {
+    // Scope solo existe aqui
+    let nombre = 'Diego Maradona';
+    console.log(nombre);
+}
+
+console.log(valorDado)
